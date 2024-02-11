b0b420fb5168e80048920b5f36b28885d3130a0d
doroyuuna <146917293+yuuna3@users.noreply.github.com>
2024-02-11 20:15:24 +0900

    correct the weather endpoint

--- ai_bot.py
+++ ai_bot.py
@@ -52,7 +52,7 @@ def init_conversation(sender):
 import requests
 
 def get_weather():
-    endpoint = "https://api.openweathermap.org/data/2.5/weather/callback"
+    endpoint = "https://api.openweathermap.org/data/2.5/weather"
     api_key = "YOUR_OPENWEATHERMAP_API_KEY"
 
     lat = 34.7123
